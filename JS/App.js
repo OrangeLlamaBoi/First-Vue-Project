2493cc1aa0daff100fc34443e2f0363fa9eca156
Damien Lamont <damienlama@gmail.com>
2021-09-07 13:44:18 +0200

    Vue App

--- JS/App.js
+++ JS/App.js
@@ -1,26 +1,3 @@
-Vue.component(`app-user`, {
-    data: function () {
-        return {
-            users: [
-                { username: "Max" },
-                { username: "Chris" },
-                { username: "Anna" }
-            ]
-        };
-    },
-    template: `
-    <div>
-        <div class="user" v-for="user in users">
-            <p>Username: {{ user.username }}</p>
-        </div>
-    </div>
-    `
-});
-
-Vue.filter(`uppercase`, function (value) {
-    return value.toUpperCase();
-});
-
 new Vue({
     el: "#app",
     data: {
