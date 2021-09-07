f1a8f6b7def4a5c377f28e6747803aa4c5f999e7
Damien Lamont <damienlama@gmail.com>
2021-09-07 15:06:00 +0200

    props

--- first-project/src/main.js
+++ first-project/src/main.js
@@ -1,7 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import Message from './Message.vue';
 
-Vue.config.productionTip = false
+Vue.component("app-message", Message);
 
 new Vue({
   render: h => h(App),
